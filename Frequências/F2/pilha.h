39a81e348ef9c460980955fc6aa5350a4aa22364
matheusranzani <ranzani.matheus@gmail.com>
2022-01-25 19:15:51 -0300

    F2 - Pilha

--- Frequências/F2/pilha.h
+++ Frequências/F2/pilha.h
@@ -1,40 +1,70 @@
-#include <stdio.h>
+#define TAMANHO 5 // Tamanho máximo da pilha
 
-#define TAMANHO 5
-
+// Declaração da pilha
 typedef struct Pilha {
     int elementos[TAMANHO];
     int topo;
 } Pilha;
 
+// Função que inicializa uma pilha vazia 
 void cria(Pilha *p) {
-    p->topo = -1;
+    p->topo = -1; 
 }
 
+// Função que retorna 1 se a pilha está vazia e 0 caso contrário 
 int vazia(Pilha *p) {
     return p->topo == -1 ? 1 : 0;
 }
 
+// Função que retorna 1 se a pilha está cheia e 0 caso contrário 
 int cheia(Pilha *p) {
     return p->topo == TAMANHO - 1 ? 1 : 0;
 }
 
+// Função que adiciona um elemento à pilha caso ela não esteja cheia
 void empilha(Pilha *p, int x, int *deu_certo) {
     if (cheia(p) == 1) {
         *deu_certo = 0;
+    } else {
+        p->topo++;
+        p->elementos[p->topo] = x;
+        *deu_certo = 1;
     }
-
-    p->topo++;
-    p->elementos[p->topo] = x;
-    *deu_certo = 1;
 }
 
+// Função que remove o elemento no topo da pilha caso ela não esteja vazia
 void desempilha(Pilha *p, int *x, int *deu_certo) {
     if (vazia(p) == 1) {
         *deu_certo = 0;
+    } else {
+        *x = p->elementos[p->topo];
+        p->topo--;
+        *deu_certo = 1;
     }
+}
 
-    *x = p->elementos[p->topo];
-    p->topo--;
-    *deu_certo = 1;
+// Função que retorna o elemento no topo da pilha
+int elemento_topo(Pilha *p, int *deu_certo) {
+    if (vazia(p) == 1) {
+        *deu_certo = 0;
+    } else {
+        int topo;
+        
+        desempilha(p, &topo, deu_certo);
+        empilha(p, topo, deu_certo);
+
+        *deu_certo = 1;
+
+        return topo;
+    }
+}
+
+// Função que retorna a quantidade total de elementos da pilha
+int quantidade_elementos(Pilha *p, int *deu_certo) {
+    if (vazia(p) == 1) {
+        *deu_certo = 0;
+    } else {
+        *deu_certo = 1;
+        return p->topo + 1;
+    }
 }
--- Frequências/F2/usa_pilha.c
+++ Frequências/F2/usa_pilha.c
@@ -1,14 +1,17 @@
-#include "pilha.h"
+#include "pilha.h" // Inclui a pilha
 
-#include <stdlib.h>
+#include <stdio.h>
 
+// Função que imprime os elementos atuais da pilha 
 void imprime(Pilha *p) {
     Pilha p_aux;
     int x;
     int ok;
 
+    // Cria uma pilha auxiliar para realizar a impressãos dos elementos
     cria(&p_aux);
 
+    // Adiciona os elementos da pilha para a pilha auxiliar
     while (vazia(p) == 0) {
         desempilha(p, &x, &ok);
 
@@ -19,6 +22,7 @@ void imprime(Pilha *p) {
 
     printf("Imprimindo a pilha: ");
 
+    // Imprime os elementos da pilha auxiliar enquanto até ela ficar vazia
     while (vazia(&p_aux) == 0) {
         desempilha(&p_aux, &x, &ok);
 
@@ -31,6 +35,7 @@ void imprime(Pilha *p) {
     printf("<- Topo\n");
 }
 
+// Função main que usa as funçẽos da pilha
 int main() {
     Pilha p;
     int ok;
@@ -39,15 +44,17 @@ int main() {
     cria(&p);
 
     printf("\nTentando empilhar 6 elementos em uma Pilha que so cabem 5\n");
+
     for (int i = 10; i <= 60; i += 10) {
         empilha(&p, i, &ok);
         imprime(&p);
     }
 
-    printf("\nPressione uma tecla... \n");
-    system("pause");
+    printf("Pressione uma tecla... ");
+    getchar();
 
-    printf("\nTentando desempilhar 6 elementos em uma Pilha que so tem 5");
+    printf("\nTentando desempilhar 6 elementos em uma Pilha que so tem 5\n");
+
     int j = 1;
     while (j <= 6) {
         desempilha(&p, &a, &ok);
@@ -55,8 +62,24 @@ int main() {
         j++;
     }
 
-    printf("\nPressione uma tecla... \n");
-    system("pause");
+    printf("Pressione uma tecla... ");
+    getchar();
+
+    printf("\nAdicionando os valores 5 e 15 à pilha\n");
+
+    empilha(&p, 5, &ok);
+    empilha(&p, 15, &ok);
+    imprime(&p);
+
+    int topo = elemento_topo(&p, &ok);
+    if (ok) {
+        printf("\nElemento no topo da pilha: %d\n", topo);
+    }   
+
+    int total = quantidade_elementos(&p, &ok);
+    if (ok) {
+        printf("Total de elementos na pilha atual: %d\n", total);
+    }
 
     return 0;
 }
